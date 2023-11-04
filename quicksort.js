66ec46326b82444aaecc0d394a2c50dd5bfbf80a
Bobby Naluz <bnaluz9@gmail.com>
2023-11-04 18:00:14 -0400

    solved

--- quicksort.js
+++ quicksort.js
@@ -1,20 +1,16 @@
 function quicksort(arr) {
-
   // Check if the input is length 1 or less
-    // If so, it's already sorted: return
+  if (arr.length <= 1) return arr;
 
-  // Pick the first value as the pivot
+  let pivot = arr[0];
+  let left = [];
+  let right = [];
 
-  // Orient the pivot so that...
-      // every number smaller than the pivot is to the left
-      // every number larger (or equal) than the pivot is to the right
+  for (let i = 1; i < arr.length; i++) {
+    arr[i] < pivot ? left.push(arr[i]) : right.push(arr[i]);
+  }
 
-  // Recursively sort the left
-  // Recursively sort the right
-
-  // Return the left, pivot and right in sorted order
-
+  return quicksort(left).concat(pivot, quicksort(right));
 }
-
 
 module.exports = [quicksort];
